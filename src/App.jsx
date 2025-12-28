9b1b140dbd71171a94f5b4f8a5cd847fab69b72e
mahmoud01140 <mmarouf373@gmail.com>
2025-12-28 22:52:10 +0200

    first commit2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import { CallingState, StreamCall, StreamVideo, StreamVideoClient, useCall, useCallStateHooks, StreamTheme, ParticipantView } from '@stream-io/video-react-sdk';
+import {
+  CallControls,
+  CallingState,
+  SpeakerLayout,
+  StreamCall,
+  StreamTheme,
+  StreamVideo,
+  StreamVideoClient,
+  useCallStateHooks,
+
+} from '@stream-io/video-react-sdk';
+
 import '@stream-io/video-react-sdk/dist/css/styles.css';
+import './index.css';
 
+const apiKey = 'mmhfdzb5evj2';
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3Byb250by5nZXRzdHJlYW0uaW8iLCJzdWIiOiJ1c2VyL0JldmVsX0Jyb3duaWUiLCJ1c2VyX2lkIjoiQmV2ZWxfQnJvd25pZSIsInZhbGlkaXR5X2luX3NlY29uZHMiOjYwNDgwMCwiaWF0IjoxNzY2OTI3NDcxLCJleHAiOjE3Njc1MzIyNzF9.rMg70gjlfhNIzvJQS_4vhqMVGcjT5bJq3_Z9BsxJvc0';
+const userId = 'Bevel_Brownie';
+const callId = '60eKGs1mzBenkQrFHX6gO';
 
-// set up the user object
 const user = {
   id: userId,
-  name: 'mahmoud',
-  image: 'https://getstream.io/random_svg/?id=mahmoud&name=mahmoud',
+  name: 'Oliver',
+  image: 'https://getstream.io/random_svg/?id=oliver&name=Oliver',
 };
 
 const client = new StreamVideoClient({ apiKey, user, token });
@@ -24,13 +39,8 @@ export default function App() {
 }
 
 export const MyUILayout = () => {
-  const call = useCall(callId);
-
-  const { useCallCallingState, useParticipantCount, useRemoteParticipants, useLocalParticipant, } = useCallStateHooks();
+  const { useCallCallingState } = useCallStateHooks();
   const callingState = useCallCallingState();
-  const participantCount = useParticipantCount();
-  const remoteParticipants = useRemoteParticipants();
-  const localParticipant = useLocalParticipant();
 
   if (callingState !== CallingState.JOINED) {
     return <div>Loading...</div>;
@@ -38,41 +48,8 @@ export const MyUILayout = () => {
 
   return (
     <StreamTheme>
-      <MyParticipantList participants={remoteParticipants} />
-      <MyFloatingLocalParticipant participant={localParticipant} />
+      <SpeakerLayout participantsBarPosition='bottom' />
+      <CallControls />
     </StreamTheme>
-  );
-};
-
-
-export const MyParticipantList = ({ participants }) => {
-  return (
-    <div style={{ display: 'flex', flexDirection: 'row', gap: '8px' }}>
-      {participants.map((participant) => (
-        <ParticipantView participant={participant} key={participant.sessionId} />
-      ))}
-    </div>
-  );
-};
-
-export const MyFloatingLocalParticipant = ({ participant }) => {
-  if (!participant) {
-    return <p>Error: No local participant</p>;
-  }
-
-  return (
-    <div
-      style={{
-        position: 'absolute',
-        top: '15px',
-        left: '15px',
-        width: '240px',
-        height: '135px',
-        boxShadow: 'rgba(0, 0, 0, 0.1) 0px 0px 10px 3px',
-        borderRadius: '12px',
-      }}
-    >
-      <ParticipantView participant={participant} />
-    </div>
   );
 };
